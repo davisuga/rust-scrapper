833a4170586653fb4fb09828b9cb0a34e53972c5
Davi Suga <daviciencia1@gmail.com>
2021-03-17 18:21:25 -0300

    feat(main.rs): added functions to parse html as xml instead of using a selector.

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,135 @@
-use futures::{stream, StreamExt}; // 0.3.8
+use futures::{
+    stream::{self, BufferUnordered, Iter, Map},
+    StreamExt,
+}; // 0.3.8
 
-use reqwest::Client; // 0.10.9
+use reqwest::{Client, Error}; // 0.10.9
 use select::document::Document;
 use select::predicate::{Attr, Class, Name, Predicate};
-use serde;
+use serde::{self, Deserialize, Serialize};
 
-use tokio; // 0.2.24, features = ["macros"]
+use std::{borrow::Borrow, vec::IntoIter};
+
+#[path = "./types/olx-data.rs"]
+pub mod olx_posts;
+use olx_posts::OlxPost;
+
+#[macro_use]
+extern crate pipeline;
+use tokio::{self, task::JoinHandle};
 extern crate futures;
-
 use serde_json::Value;
+
+#[path = "./types/olx-results.rs"]
+pub mod olx_results;
+use olx_results::OlxResults;
+extern crate quick_xml;
+
+use quick_xml::{
+    events::{attributes::Attribute, Event},
+    Reader,
+};
 
 const CONCURRENT_REQUESTS: usize = 4;
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let links = vec!["https://mg.olx.com.br/belo-horizonte-e-regiao/computadores-e-acessorios/monitor-curvo-samsung-24-fhd-hdmi-vga-freesync-preto-serie-cf390-853157530", "https://mg.olx.com.br/belo-horizonte-e-regiao/computadores-e-acessorios/monitor-27-philips-widescreen-4k-uhd-ips-mostruario-nota-garantia-838877513"];
-    let client = Client::new();
-    let raw_buffer = stream::iter(links)
-        .map(|link| {
-            let client = client.clone();
-            tokio::spawn(async move {
-                // println!("spawning task...");
-                let resp = client.get(link).send().await?;
-                resp.text().await
-            })
-        })
-        .buffer_unordered(CONCURRENT_REQUESTS);
+    let olx_results = search_olx("escorredor")
+        .await
+        .expect("Failed to retrieve results");
+    let olx_data = parse_olx_page::<OlxResults>(&olx_results);
+    println!("OLX DATA: {:#?}", olx_data);
 
-    raw_buffer
-        .for_each(|result| async {
-            match result {
-                Ok(Ok(result)) => {
-                    // println!("Got {} bytes", result.len());
-                    parse_html(result)
-                }
-                Ok(Err(e)) => eprintln!("Got a reqwest::Error: {}", e),
-                Err(e) => eprintln!("Got a tokio::JoinError: {}", e),
-            }
-        })
-        .await;
-    Ok(())
+    return Ok(());
 }
-// fn handle_response(response: Response) {}
-fn parse_html(document_string: String) {
-    let document_slice_string = &document_string[..];
-    let document = Document::from(document_slice_string);
-    let script_tags = document.select(Attr("id", "initial-data"));
-    let mut nodes = script_tags
-        .map(|tag| {
-            let json = tag.attr("data-json").unwrap();
-            println!("{}", json);
-            let parsed: serde_json::Result<Value> = serde_json::from_str(json);
 
-            return parsed;
-        })
-        .collect::<Vec<_>>();
+fn parse_script_tag<T: Deserialize<'static>>(
+    tag: select::document::Select<'static, Attr<&str, &str>>,
+) -> Option<T> {
+    let mut nodes = tag.map(|tag| {
+        let json = tag.attr("data-json").unwrap();
+        let parsed: T = serde_json::from_str(json).expect("Failed to parse json");
+        return parsed;
+    });
+    // .collect::<Vec<_>>();
+
+    let possible_script_tag = nodes.next();
+    return possible_script_tag;
+}
+fn get_safe_data_json_value(
+    possible_attr: Option<Result<Attribute, quick_xml::Error>>,
+) -> Option<Attribute> {
+    match possible_attr {
+        Some(Ok(safe)) => Some(safe),
+        Some(Err(err)) => {
+            println!("Failed to parse json: {:?}", err);
+            None
+        }
+        None => None,
+    }
+}
+fn parse_olx_page<T: Deserialize<'static>>(xml: &str) -> Option<T> {
+    let mut reader = Reader::from_str(xml);
+    reader.trim_text(true);
+
+    let mut count = 0;
+    let mut txt: Vec<T> = Vec::new();
+    let mut buf = Vec::new();
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref element)) => match element.name() {
+                b"script" => {
+                    let element_attr = element.attributes().last();
+                    println!("Element attr: {:#?}", element_attr);
+                    get_safe_data_json_value(element_attr);
+                }
+                _ => (),
+            },
+            Ok(Event::Eof) => break None,
+            Err(element) => panic!(
+                "Error at position {}: {:?}",
+                reader.buffer_position(),
+                element
+            ),
+            _ => (),
+        }
+        buf.clear();
+    }
+}
+
+async fn search_olx(term: &str) -> Result<String, Error> {
+    let olx_search_url = format!("https://mg.olx.com.br/belo-horizonte-e-regiao?q={}", term);
+    let client = Client::new();
+    let result = client.get(&olx_search_url).send().await.unwrap();
+    let text_result = result.text().await;
+    return text_result;
+}
+
+fn output_search(search_results: Option<Value>) {
+    match search_results {
+        Some(results) => println!("Got {}", results),
+        None => println!("No results"),
+    }
+}
+
+fn filter_search_results(search_results: Option<&OlxResults>) {
+    match search_results {
+        Some(results) => {
+            println!("{:#?}", results);
+            let filtered_results = results
+                .listing_props
+                .ad_list
+                .iter()
+                //     .filter(|result| result["price"].is_string());
+                // let f = filtered_results
+                .map(|ad| {
+                    println!("GOT {:?}", ad.url)
+                    // println!(
+                    //     "Title: {:?} \n Price: {:?} \n   Link: {:?}",
+                    //     ad["subject"], ad["price"], ad["url"]
+                    // )
+                });
+        }
+        None => println!("No results"),
+    }
 }
