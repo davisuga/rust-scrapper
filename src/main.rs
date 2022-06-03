c935cbf5f4c638b41b3c81521070c2a0d74b5b4c
Davi Suga <daviciencia1@gmail.com>
2022-06-03 08:42:12 -0300

    fixed error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-#![feature(associated_type_bounds)]
-#![feature(option_result_unwrap_unchecked)]
+use std::sync::Arc;
+
 use futures::future::*; // 0.3.8
 extern crate regex;
 use regex::Regex;
 
 use reqwest::{Client, Error}; // 0.10.9
-use tokio;
 extern crate futures;
 use serde_json::Value;
 
@@ -14,17 +13,14 @@ pub mod olx_results;
 use olx_results::OlxResults;
 
 fn main() {
-    let a = async move {
-        search("sas").await;
-    };
-    println!("runs stuff, {:?}", a)
+    Arc::new(async {
+        let a = search("sas").await;
+        println!("runs stuff, {:?}", a)
+    });
 }
 async fn search(term: &str) -> Vec<Value> {
     let olx_results = get_olx_results(term).await;
-    let all_results_data = olx_results
-        .iter()
-        .map(|ad_url| get_ad_data(&ad_url[..]))
-        .collect::<Vec<_>>();
+    let all_results_data = olx_results.iter().map(|ad_url| get_ad_data(&ad_url[..]));
     let joined_data = join_all(all_results_data).await;
     println!("joined {:#?} results", joined_data.len());
     joined_data
@@ -42,8 +38,12 @@ async fn get_olx_results(term: &str) -> Vec<String> {
 
 async fn get_ad_data(link: &str) -> Value {
     let client = Client::new();
-    let result = client.get(link).send().await.unwrap();
-    let text_result = result.text().await.unwrap();
+    let result = client
+        .get(link)
+        .send()
+        .await
+        .expect("Failed to get ad data");
+    let text_result = result.text().await.expect("Failed to get ad data");
     let raw_data = get_data_json(&text_result[..]);
     let json_data: Result<Value, serde_json::Error> = serde_json::from_str(&raw_data[..]);
     return json_data.unwrap_or_default();
@@ -52,7 +52,7 @@ async fn get_ad_data(link: &str) -> Value {
 fn get_data_json(xml: &str) -> String {
     let regex = Regex::new(r#"data-json="(.+)">.+/script>"#).unwrap();
     let result = regex.captures(xml);
-    let parsed_result = &result.unwrap()[0];
+    let parsed_result = &result.expect("Regex got no results")[0];
     let begin = 11;
     let end = parsed_result.len() - 11;
     let sliced_result = &parsed_result[begin..end];
@@ -76,7 +76,7 @@ fn filter_search_results(search_results: &OlxResults) -> Vec<String> {
             .ad_list
             .iter()
             .nth(0)
-            .unwrap()
+            .expect("Failed to get first url")
             .url
     );
     let filtered_results: Vec<String> = search_results
